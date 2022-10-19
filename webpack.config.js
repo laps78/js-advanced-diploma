11ffca6463cfbe1070468cd126b24e3831810435
laps78 <9254384@mail.ru>
2022-10-19 12:15:20 +0300

    webpack nonfig entry file string modified

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
       {
         test: /\.css$/,
         use: [
