afbd4a3f360825e9766d799ccfe730b04a697c71
laps78 <9254384@mail.ru>
2022-10-25 13:38:01 +0300

    generators.js tested

--- src/js/__tests__/generators.test.js
+++ src/js/__tests__/generators.test.js
@@ -5,7 +5,7 @@ import Daemon from '../characters/Daemon';
 import Undead from '../characters/Undead';
 import Vampire from '../characters/Vampire';
 import { characterGenerator, generateTeam } from '../generators';
-import Character from '../Character';
+import Character from '../characters/Character';
 
 const aviableCharacterClasses = [
   Swordsman,
@@ -19,12 +19,18 @@ const aviableCharacterClasses = [
 const newCharacterGenerator = characterGenerator(aviableCharacterClasses, 4);
 // forming test array suites
 const testArray = [];
-for (let i = 0; i <= 10; i += 1) {
+for (let i = 0; i <= 15; i += 1) {
   testArray.push(newCharacterGenerator.next().value);
 }
 
 test.each([
   testArray,
 ])('выдаёт ли генератор characterGenerator бесконечно новые персонажи из списка (учёт аргумента allowedTypes)', (call) => {
-  expect(() => call).not.toThrow('[characterGeneratorError] Unable to generate character: generated type could not be created!');
+  expect(call).toBeInstanceOf(Character);
+});
+
+test.each([
+  [generateTeam(aviableCharacterClasses, 4, 1), 4],
+])('тест учета аргумента maxLevel', (call, maxLevel) => {
+  expect(call.characters[0].level).toBeLessThan(maxLevel);
 });
