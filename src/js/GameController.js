1dc9fd3fe114a3035442f0703c5329ccdbaf836d
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2022-11-03 02:30:56 +0300

    continue trying to define allowed moves & attacks

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -20,7 +20,22 @@ export default class GameController {
       playerTeamAllowedTypes: [Swordsman, Bowman, Magician], // allowed characters for player's team
       cpuTeamAllowedTypes: [Daemon, Undead, Vampire], // allowed characters for CPU's team
       playerAllowedPositions: [
-        0, 1, 8, 9, 16, 17, 24, 25, 32, 33, 40, 41, 48, 49, 56, 57, // possible start positions for player's team
+        0,
+        1,
+        8,
+        9,
+        16,
+        17,
+        24,
+        25,
+        32,
+        33,
+        40,
+        41,
+        48,
+        49,
+        56,
+        57, // possible start positions for player's team
       ],
       cpuAllowedPositions: [
         6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63, // possible start positions for CPU's team
@@ -31,6 +46,7 @@ export default class GameController {
     this.players = ['player', 'cpu'];
     this.turn = this.players[0];
     this.selectedCell = null;
+    this.fieldBorders = this.defineFieldBorders();
   }
 
   init() {
@@ -74,6 +90,7 @@ export default class GameController {
     this.gamePlay.addCellEnterListener(this.onCellEnter.bind(this));
     this.gamePlay.addCellLeaveListener(this.onCellLeave.bind(this));
     this.gamePlay.addCellClickListener(this.onCellClick.bind(this));
+
     // TODO: load saved stated from stateService
   }
 
@@ -111,15 +128,87 @@ export default class GameController {
     this.gamePlay.setCursor('auto');
   }
 
+  /**
+   * If there's character in cell returns it
+   * @param {*} index index of testing cell
+   * @returns character from allCharactersOnMap array
+   */
   detectCharacterInCell(index) {
     const characterInCell = this.allCharactersOnMap.find((character) => character.position === index);
     return characterInCell;
   }
 
   showCharacterInCellInfo(index) {
-    const characterInCell = this.detectCharacterInCell(index);
-    const message = `🎖 ${characterInCell.character.level} ⚔ ${characterInCell.character.attack} 🛡 ${characterInCell.character.defence} ♥ ${characterInCell.character.health}`;
+    const characterInCell = this.detectCharacterInCell(index).character;
+    const message = `🎖 ${characterInCell.level} ⚔ ${characterInCell.attack} 🛡 ${characterInCell.defence} ♥ ${characterInCell.health}`;
     return message;
+  }
+
+  /**
+   * @returns array of border's cells indexes
+   */
+  defineFieldBorders() {
+    const borderIndexes = [];
+    this.gamePlay.cells.forEach((cell, index) => {
+      if (cell.classList.contains('map-tile-top-left')
+        || cell.classList.contains('map-tile-top-right')
+        || cell.classList.contains('map-tile-bottom-left')
+        || cell.classList.contains('map-tile-bottom-right')
+        || cell.classList.contains('map-tile-top')
+        || cell.classList.contains('map-tile-bottom')
+        || cell.classList.contains('map-tile-left')
+        || cell.classList.contains('map-tile-right')) {
+        borderIndexes.push(index);
+      }
+      // if (cell.classList.contains('map-tile-top-left')
+      //  || cell.classList.contains('map-tile-top-right')
+      //  || cell.classList.contains('map-tile-bottom-left')
+      //  || cell.classList.contains('map-tile-bottom-right')
+      //  || cell.classList.contains('map-tile-top')
+      //  || cell.classList.contains('map-tile-bottom')
+      //  || cell.classList.contains('map-tile-left')
+      //  || cell.classList.contains('map-tile-right'))
+    });
+    return borderIndexes;
+  }
+
+  /**
+   * This method defines allowed moves for selected character
+   * @param index index of selected cell
+   * @returns array of allowed moves cells indexes
+   */
+  defineAllowedMoves(index) {
+    const characterInCell = this.detectCharacterInCell(index).character;
+    const allowedMoves = [];
+    for (let step = 1; step <= characterInCell.moveRange; step += 1) {
+      for (let i = 0; i < 8; i += 1) {
+        // go up
+        // go up-right
+        // go right
+        // go right-down
+        // go down
+        // go left-down
+        // go left
+        // go left-up
+        // exclude occupied cells
+        // exclude steps out of borders
+      }
+    }
+    return allowedMoves;
+  }
+
+  /**
+   * This method defines allowed attacks for selected character
+   * @param index index of selected cell
+   * @returns array of allowed attacks cells indexes
+   */
+  defineAllowedAttacks(index) {
+    const characterInCell = this.detectCharacterInCell(index).character;
+    const allowedAttacks = [];
+    for (let step = 1; step <= characterInCell.attackRange; step += 1) {
+      // some code
+    }
+    return allowedAttacks;
   }
 
   changeTurn() {
